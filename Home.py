a65952ca51ccf6e8d12d3e70cd53517d59402a9b
Megahed24 <93885381+Megahed24@users.noreply.github.com>
2022-09-24 20:14:38 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title('Hello')
+
+st.write('this is a test')
+
+st.markdown(''' 
+            * this is a test!
+            * this is a test 
+            * check check''')
+
+st.markdown(''' 
+            1- this is a test!
+
+            2-this is a test
+
+            3-check check''')
